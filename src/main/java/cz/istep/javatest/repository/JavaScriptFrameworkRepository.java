0452c82f756a623a63e34a42da36df72d9a4ce77
ged9 <ged@seznam.cz>
2023-09-03 18:28:10 +0200

    Delete src directory

--- src/main/java/cz/istep/javatest/repository/JavaScriptFrameworkRepository.java
+++
@@ -1,9 +0,0 @@
-package cz.istep.javatest.repository;
-
-
-import cz.istep.javatest.data.JavaScriptFramework;
-import org.springframework.data.repository.CrudRepository;
-
-public interface JavaScriptFrameworkRepository extends CrudRepository<JavaScriptFramework, Long> {
-
-}
